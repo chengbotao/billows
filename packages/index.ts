5586184b7c380cf539676ca2d34a6a84fcef7799
chengbotao <chengbotao5221@163.com>
2022-06-08 02:09:03 +0800

    chore: 初始化工程

---
+++ packages/index.ts
@@ -0,0 +1,4 @@
+/*
+ * @Author: Chengbotao
+ * @Date: 2022-06-05 18:07:13
+ */
---
+++ rollup.config.ts
@@ -0,0 +1,54 @@
+/*
+ * @Author: Chengbotao
+ * @Date: 2022-06-05 18:00:17
+ */
+import { resolve } from 'path';
+import pkg from './package.json';
+
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import alias from '@rollup/plugin-alias';
+import eslint from '@rollup/plugin-eslint';
+import typescript from '@rollup/plugin-typescript';
+
+const { version } = pkg;
+const outputConf = {
+  banner: `/* Billows version ${version} */`,
+  footer: `/* Follow me on GitHub! @chengbotao */`,
+};
+
+export default {
+  input: 'packages/index.ts',
+  output: [
+    {
+      file: resolve(__dirname, 'dist', 'billows.esm.js'),
+      format: 'esm',
+      ...outputConf,
+    },
+    {
+      file: resolve(__dirname, 'dist', 'billows.js'),
+      format: 'umd',
+      name: 'billows',
+      ...outputConf,
+    },
+  ],
+  plugins: [
+    alias({
+      entries: {
+        packages: resolve(__dirname, 'packages'),
+      },
+    }),
+    nodeResolve({
+      extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json'],
+    }),
+    commonjs(),
+    eslint({
+      fix: true,
+      include: ['packages/**/*.ts'],
+      exclude: ['node_modules/**']
+    }),
+    typescript(),
+    json(),
+  ],
+};
