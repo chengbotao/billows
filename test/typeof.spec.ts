18702d018aee2935ead5fd15a81330fc57fc1737
chengbotao <chengbotao5221@163.com>
2022-06-10 15:06:41 +0800

    test(typeOf): 更新 typeOf 单元测试

--- test/typeof.spec.ts
+++ test/typeof.spec.ts
@@ -2,37 +2,41 @@
  * @Author: Chengbotao
  * @Date: 2022-06-08 17:04:44
  */
+
+/* eslint-disable no-undef */
 import { typeOf } from '../packages/typeof';
 
-test('typeOf string', () => {
-    expect(typeOf('chengbotao')).toBe('string');
-});
-test('typeOf number', () => {
-    expect(typeOf(28)).toBe('number');
-});
-test('typeOf boolean', () => {
-    expect(typeOf(true)).toBe('boolean');
-    expect(typeOf(false)).toBe('boolean');
-});
-test('typeOf undefined', () => {
-    expect(typeOf(undefined)).toBe('undefined');
-});
-test('typeOf null', () => {
-    expect(typeOf(null)).toBe('null');
-});
-test('typeOf date', () => {
-    expect(typeOf(new Date())).toBe('date');
-});
-test('typeOf array', () => {
-    expect(typeOf([])).toBe('array');
-});
-test('typeOf object {}', () => {
-    expect(typeOf({})).toBe('object');
-});
-test('typeOf function', () => {
-    function fun() {}
-    expect(typeOf(fun)).toBe('function');
-});
-test('typeOf promise', () => {
-    expect(typeOf(Promise.resolve())).toBe('promise');
+describe('类型(typeOf)', () => {
+    test('typeOf("chengbotao") => "string"', () => {
+        expect(typeOf('chengbotao')).toBe('string');
+    });
+    test('typeOf(28) => "number"', () => {
+        expect(typeOf(28)).toBe('number');
+    });
+    test('typeOf(true) => "boolean"', () => {
+        expect(typeOf(true)).toBe('boolean');
+        expect(typeOf(false)).toBe('boolean');
+    });
+    test('typeOf(undefined) => "undefined"', () => {
+        expect(typeOf(undefined)).toBe('undefined');
+    });
+    test('typeOf(null) => "null"', () => {
+        expect(typeOf(null)).toBe('null');
+    });
+    test('typeOf(new Date()) => "date"', () => {
+        expect(typeOf(new Date())).toBe('date');
+    });
+    test('typeOf([]) => "array"', () => {
+        expect(typeOf([])).toBe('array');
+    });
+    test('typeOf({}) => "object"', () => {
+        expect(typeOf({})).toBe('object');
+    });
+    test('typeOf(function fun() {}) => "function', () => {
+        function fun() {}
+        expect(typeOf(fun)).toBe('function');
+    });
+    test('typeOf(Promise.resolve()) => "promise"', () => {
+        expect(typeOf(Promise.resolve())).toBe('promise');
+    });
 });
