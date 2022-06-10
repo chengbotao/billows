e2421a54ea3e9b94740bdc295adbb0a39959b543
chengbotao <chengbotao5221@163.com>
2022-06-10 15:08:29 +0800

    test(deepClone): 更新 deepClone 单元测试

---
+++ test/deepclone.spec.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: Chengbotao
+ * @Date: 2022-06-10 14:49:25
+ */
+/* eslint-disable no-undef */
+import { deepClone } from '../packages/deepclone';
+
+describe('深克隆(deepClone)', () => {
+    const obj = {
+        name: 'chengbotao',
+        age: 28,
+    };
+
+    test('deepClone(honda, Car) => true', () => {
+        expect(deepClone(obj)).toEqual({ name: 'chengbotao', age: 28 });
+    });
+});
