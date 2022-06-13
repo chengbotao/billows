b1ca43be71c2784d317f5450f6a922d926fbe6e9
chengbotao <chengbotao5221@163.com>
2022-06-13 14:04:44 +0800

    refactor(instanceOf): 更新 instanceof 函数参数命名

--- packages/instanceof.ts
+++ packages/instanceof.ts
@@ -2,8 +2,8 @@
  * @Author: Chengbotao
  * @Date: 2022-06-09 14:04:42
  */
-export function instanceOf(obj: any, ctor: any): boolean {
-  let proto = Reflect.getPrototypeOf(obj);
+export function instanceOf(target: any, ctor: any): boolean {
+  let proto = Reflect.getPrototypeOf(target);
   while (proto) {
     if (proto === ctor.prototype) {
       return true;
