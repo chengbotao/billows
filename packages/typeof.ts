c9bce644b4176853f0a37e341ea6668851bf8c42
chengbotao <chengbotao5221@163.com>
2022-06-15 14:52:22 +0800

    refactor(typeOf): 修改参数命名

--- packages/typeof.ts
+++ packages/typeof.ts
@@ -2,9 +2,9 @@
  * @Author: Chengbotao
  * @Date: 2022-06-08 14:36:02
  */
-export function typeOf(val: any): string {
+export function typeOf(operand: any): string {
   const toString = Object.prototype.toString;
-  let type = toString.call(val).split(' ')[1];
+  let type = toString.call(operand).split(' ')[1];
   type = type.substring(0, type.length - 1).toLowerCase();
   return type;
 }
