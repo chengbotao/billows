d3428ed13a515bf474493abebd76fdc0a2e88bb4
chengbotao <chengbotao5221@163.com>
2022-06-24 17:50:23 +0800

    docs(index.d.ts): 更新 index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,6 +7,13 @@ export declare interface DuckTyping {
   [key: string]: any;
 }
 
+export declare interface JsonpOpts {
+  url: string;
+  callbackName: string;
+  params?: any;
+  paramsSerializer?: (params: any) => string;
+}
+
 export declare function typeOf(val: any): string;
 export declare function instanceOf(obj: any, ctor: any): boolean;
 export declare function deepClone(target: any, map?: WeakMap<object, any>): any;
