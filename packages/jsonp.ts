9d5a2e018214bba7fd4d40bf00ad34acdbefe23e
chengbotao <chengbotao5221@163.com>
2022-06-24 17:37:50 +0800

    feat(jsonp): promsie封装jsonp

---
+++ packages/jsonp.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: Chengbotao
+ * @Date: 2022-06-24 14:18:02
+ */
+
+import { JsonpOpts } from '../types';
+import { buildURL } from './buildurl';
+
+export function jsonp(options: JsonpOpts) {
+  const { url, callbackName, params, paramsSerializer } = options;
+  return new Promise((resolve, reject) => {
+    const scriptEl = document.createElement('script');
+    scriptEl.src = buildURL(url, params, paramsSerializer);
+    document.body.appendChild(scriptEl);
+    (<any>window)[callbackName] = (data: any) => {
+      resolve(data);
+      document.removeChild(scriptEl);
+    };
+  });
+}
